8627662abed110eba3461afc75248764d90c8395
Kensy Josabeth Ayala <kensy.ayala1994@gmail.com>
2023-08-29 11:10:18 -0600

    Initial Commit

--- Domain/Data/Genre.cs
+++ Domain/Data/Genre.cs
@@ -11,6 +11,7 @@ namespace Domain.Data
     [Table("Genres", Schema = "data")]
     public class Genre : Entity
     {
+
         public string Name { get; set; }
         public string Description { get; set; }
     }
